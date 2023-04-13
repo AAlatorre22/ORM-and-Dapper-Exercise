a0408561e1200d1dc076df2498e18bcc5a42a384
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-04-13 08:43:30 -0700

    Completed #2

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Threading.Channels;
 
 namespace ORM_Dapper
 {
@@ -14,23 +15,116 @@ namespace ORM_Dapper
                 .Build();
 
             string connString = config.GetConnectionString("DefaultConnection");
-
             IDbConnection conn = new MySqlConnection(connString);
 
-            var repo = new DapperDepartmentRepository(conn);
+            #region Department
+            //var departmentRepo = new DapperDepartmentRepository(conn);
 
-            Console.WriteLine("Type a new Department name");
 
-            var newDepartment = Console.ReadLine();
+            //var departments = departmentRepo.GetAllDepartments();
 
-            repo.InsertDepartment(newDepartment);
+            //foreach (var dept in departments)
+            //{
+            //    Console.WriteLine(dept.Name);
+            //}
 
-            var departments = repo.GetAllDepartments();
+            //Console.WriteLine("Type a new Department name");
 
-            foreach (var dept in departments)
+            //var newDepartment = Console.ReadLine();
+
+            //departmentRepo.InsertDepartment(newDepartment);
+
+            //departmentRepo.GetAllDepartments();
+
+            //foreach (var dept in departments)
+            //{
+            //    Console.WriteLine(dept.Name);
+            //}
+
+            #endregion
+
+            var prodRepo = new DapperProductRepository(conn);
+
+            var products = prodRepo.GetAllProducts();
+
+            //foreach (var prod in products )
+            //{
+            //    Console.WriteLine($"{prod.ProductID} {prod.Name} {prod.Price}");
+            //}
+
+            Console.WriteLine();
+
+            //Console.WriteLine("What is name of new product?");
+            //var prodName = Console.ReadLine();
+
+            //Console.WriteLine("What is price?");
+            //var prodPrice = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("What is Cat ID?");
+            //var prodCat = int.Parse(Console.ReadLine());
+
+            //prodRepo.CreateProduct(prodName, prodPrice, prodCat);
+
+            //products = prodRepo.GetAllProducts();
+
+            //Console.WriteLine("All products\n");
+
+            //foreach (var prod in products)
+            //{
+            //    Console.WriteLine($"{prod.ProductID} {prod.Name} {prod.Price}");
+            //}
+
+
+
+
+            #region Product
+
+            var productRepo = new DapperProductRepository(conn);
+            var productToUpdate = productRepo.GetProduct(940);
+
+            productToUpdate.Name = "Updated Alejandro's Secret Stuff";
+            productToUpdate.StockLevel = 15;
+            productToUpdate.Price = 7000;
+            productToUpdate.CategoryID = 2;
+
+
+            productRepo.UpdateProduct(productToUpdate);
+
+            products = productRepo.GetAllProducts();
+
+
+            foreach (var product in products)
             {
-                Console.WriteLine(dept.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine();
+                Console.WriteLine();
+
             }
+            products = productRepo.GetAllProducts();
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID} {prod.Name} {prod.Price}");
+            }
+
+            #endregion
+
+            Console.WriteLine("What is product you want to delete?");
+            var prodID = int.Parse(Console.ReadLine());
+
+            prodRepo.DeleteProduct(prodID);
+
+            products = productRepo.GetAllProducts();
+
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID} {prod.Name} {prod.Price}");
+            }
+
         }
     }
 }
